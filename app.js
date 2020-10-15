6e7020e660e848cf2b8e23665c9f9e7010863d11
Antonio Cruz <sobtechsistemas@gmail.com>
2020-10-15 01:50:04 -0300

    mais um commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'css')))
 app.use(express.static(path.join(__dirname, 'uploads/documentos')))
 
 //Conexão com o banco de dados
-mongoose.connect('mongodb://localhost/newConemadpa',
+mongoose.connect('mongodb://localhost/conemadpa',
     {
         useUnifiedTopology: true,
         useNewUrlParser: true,
@@ -28,14 +28,15 @@ app.set('views', path.join(__dirname, 'templates'))
 
 //Rotas
 app.get('/', (req, res) => { res.render('index.ejs'); })
-app.get('/diretoria', (req, res) => { res.render('diretoria.ejs'); })
 app.get('/executivo', (req, res) => { res.render('executivo.ejs'); })
+app.get('/diretoria', (req, res) => { res.render('diretoria.ejs'); })
+app.get('/sistemaconemadpa', (req, res) => { res.render('login.ejs'); })
 app.get('/eventos', (req, res) => { res.render('eventos.ejs'); })
-app.get('/ficha-cadastro', obreiroRouter)
+app.get('/downloads', (req, res) => { res.render('downloads.ejs'); })
 
+app.get('/ficha-cadastro', obreiroRouter)
 
 app.use('/user', express.json(), userRouter);
 app.use('/obreiros', obreiroRouter)
-
 
 app.listen(PORT, () => console.log(`Server Runnning on Port: ${PORT}`))
